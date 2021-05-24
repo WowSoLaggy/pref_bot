7f604e282a49c919495c644671a8341c097b33c4
Anton Egorov <dohxehapo@gmail.com>
2021-05-25 00:25:09 +0300

    Fix

--- public/poruchik/index.php
+++ public/poruchik/index.php
@@ -126,8 +126,18 @@ function processMessage($message) {
     // incoming text message
     $text = $message['text'];
 
-    if ($text === "Дегть") {
+    if (strpos($text, "/start") === 0) {
+      apiRequestJson("sendMessage", array('chat_id' => $chat_id, "text" => 'Hello', 'reply_markup' => array(
+        'keyboard' => array(array('Hello', 'Hi')),
+        'one_time_keyboard' => true,
+        'resize_keyboard' => true)));
+    } else if ($text === "Hello" || $text === "Hi") {
+      apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'Nice to meet you'));
+    } else if (strpos($text, "/stop") === 0) {
+      // stop now
+    } else if ($text === "Дегть") {
       apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'хуй'));
+    }
   } else {
     //apiRequest("sendMessage", array('chat_id' => $chat_id, "text" => 'I understand only text messages'));
   }
