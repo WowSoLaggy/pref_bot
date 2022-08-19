4cd4cbb1e0ba41f1015860d5411d97be05bf0923
Anton Egorov <dohxehapo@gmail.com>
2022-08-19 23:13:04 +0300

    Test

--- public/blue_bathman/index.php
+++ public/blue_bathman/index.php
@@ -10,7 +10,7 @@ function processMessage($message)
 
   if (isset($message['text']))
   {
-    $text = mb_convert_case($message['text'], MB_CASE_LOWER, "UTF-8");
+    $text = mb_strtolower($message['text']);
 
     if ($text === "др")
     {
