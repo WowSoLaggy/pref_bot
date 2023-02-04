ef3a1c879904f31d5a99268c5377ce325b0251ac
Anton Egorov <dohxehapo@gmail.com>
2023-02-04 23:00:09 +0500

    (-) Fix pars

--- public/laggy_spy/send_message.php
+++ public/laggy_spy/send_message.php
@@ -7,7 +7,7 @@ include_once './../shared/tg_api.php';
 function send_message(string $text, string $chat_id)
 {
   $url = 'https://api.telegram.org/bot'.TOKEN_LAGGY_SPY.'/';
-  send_message_url($url, "sendMessage", array('chat_id' => $chat_id, "text" => $text));
+  send_message_url($url, $text, $chat_id);
 }
 
 ?>
