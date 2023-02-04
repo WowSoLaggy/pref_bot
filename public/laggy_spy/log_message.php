de8eda1121964046dccfd61dcbf6cea3b113ff6a
Anton Egorov <dohxehapo@gmail.com>
2023-02-04 22:28:24 +0500

    (-) Remove named parameters since they are not supported by php7

--- public/laggy_spy/log_message.php
+++ public/laggy_spy/log_message.php
@@ -6,7 +6,7 @@ include_once('./../shared/logger.php');
 function log_message(string $message, bool $is_auth)
 {
   $dir_path = '/var/log/laggy_spy_bot';
-  log_message_auth(message: $message, is_auth: $is_auth, dir_path: $dir_path);
+  log_message_auth($message, $is_auth, $dir_path);
 }
 
 ?>
