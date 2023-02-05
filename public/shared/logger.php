f735ffbf6ece6074d54b97d89632fc17537ebf6e
Anton Egorov <dohxehapo@gmail.com>
2023-02-05 22:59:42 +0500

    (~) Refactor logging

--- public/shared/logger.php
+++ public/shared/logger.php
@@ -4,49 +4,46 @@
 // Don't forget to create a dir for logs and set owner:
 // chmod www-data:www-data /var/log/my_folder
 
+
 function log_text(string $text)
 {
   file_put_contents(DIR_PATH.'/log_'.date("Y_m_d").'.log', $text.PHP_EOL, FILE_APPEND);
 }
 
-function log_message(array $message, bool $is_auth)
-{
-  $user_id = $message['from']['id'];
-  $chat_id = $message['chat']['id'];
-  $user_name = $message['from']['username'];
-  $is_bot = $message['from']['is_bot'];
-  $has_text = isset($message['text']);
 
+function log_data(array $from, string $chat_id, string $text, bool $is_auth)
+{
   $log = date('Y.m.d H:i:s').' ';
 
-  $log .= $user_name.' (id: '.$user_id.', chat: '.$chat_id.') ';
-  $log .= $is_bot ? '(BOT) ' : '';
+  $log .= $from['username'].' (first: '.$from['first_name'].', last: '.$from['last_name'].', id: '.$from['id'].', chat: '.$chat_id.') ';
+  $log .= $from['is_bot'] ? '(BOT) ' : '';
   $log .= '- ';
   $log .= $is_auth ? 'GRANT' : 'DENY';
   $log .= ' - ';
-  $log .= $has_text ? $message['text'] : 'NO TEXT';
+  $log .= is_null($text) ? 'NO TEXT' : $text;
 
   log_text($log);
 }
+
+
+function log_message(array $message, bool $is_auth)
+{
+  $from = $message['from'];
+  $chat_id = $message['chat']['id'];
+  $text = isset($message['text']) ? $message['text'] : null;
+
+  log_data($from, $chat_id, $text, $is_auth);
+}
+
 
 function log_callback(array $callback, bool $is_auth)
 {
-  $user_id = $callback['from']['id'];
+  $from = $callback['from'];
   $chat_id = $callback['message']['chat']['id'];
-  $user_name = $callback['from']['username'];
-  $is_bot = $callback['from']['is_bot'];
-  $has_text = isset($callback['data']);
+  $text = isset($callback['data']) ? $callback['data'] : null;
 
-  $log = date('Y.m.d H:i:s').' ';
-
-  $log .= $user_name.' (id: '.$user_id.', chat: '.$chat_id.') ';
-  $log .= $is_bot ? '(BOT) ' : '';
-  $log .= '- ';
-  $log .= $is_auth ? 'GRANT' : 'DENY';
-  $log .= ' - ';
-  $log .= $has_text ? $callback['data'] : 'NO TEXT';
-
-  log_text($log);
+  log_data($from, $chat_id, $text, $is_auth);
 }
+
 
 ?>
