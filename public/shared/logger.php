0649ad344972b62132a8623ff5a9670b2a162833
Anton Egorov <dohxehapo@gmail.com>
2023-02-05 22:33:58 +0500

    (+) Test 'process_callback'

--- public/shared/logger.php
+++ public/shared/logger.php
@@ -12,13 +12,14 @@ function log_text(string $text)
 function log_message(array $message, bool $is_auth)
 {
   $user_id = $message['from']['id'];
+  $chat_id = $callback['chat']['id'];
   $user_name = $message['from']['username'];
   $is_bot = $message['from']['is_bot'];
   $has_text = isset($message['text']);
 
   $log = date('Y.m.d H:i:s').' ';
 
-  $log .= $user_name.' ('.$user_id.') ';
+  $log .= $user_name.' (id: '.$user_id.', chat: '.$chat_id.') ';
   $log .= $is_bot ? '(BOT) ' : '';
   $log .= '- ';
   $log .= $is_auth ? 'GRANT' : 'DENY';
@@ -30,7 +31,22 @@ function log_message(array $message, bool $is_auth)
 
 function log_callback(array $callback)
 {
-  log_text(json_encode($callback));
+  $user_id = $callback['from']['id'];
+  $chat_id = $callback['chat']['id'];
+  $user_name = $callback['from']['username'];
+  $is_bot = $callback['from']['is_bot'];
+  $has_text = isset($callback['text']);
+
+  $log = date('Y.m.d H:i:s').' ';
+
+  $log .= $user_name.' (id: '.$user_id.', chat: '.$chat_id.') ';
+  $log .= $is_bot ? '(BOT) ' : '';
+  $log .= '- ';
+  $log .= $is_auth ? 'GRANT' : 'DENY';
+  $log .= ' - ';
+  $log .= $has_text ? $callback['text'] : 'NO TEXT';
+
+  log_text($log);
 }
 
 ?>
