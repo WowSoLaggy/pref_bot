60eaf4deb586f72969756c6b844e3a7c3f426438
Anton Egorov <dohxehapo@gmail.com>
2023-02-11 23:49:29 +0500

    (-) Fix command parsing

--- public/laggy_spy/index.php
+++ public/laggy_spy/index.php
@@ -9,7 +9,6 @@ require_once __DIR__.'/users.php';
 require_once __DIR__.'/../shared/commands.php';
 require_once __DIR__.'/../shared/keyboard.php';
 require_once __DIR__.'/../shared/logger.php';
-require_once __DIR__.'/../shared/utils.php';
 
 
 function process(string $user_id, string $chat_id, string $text = null) : bool
@@ -23,10 +22,16 @@ function process(string $user_id, string $chat_id, string $text = null) : bool
     return false;
   }
 
+  $tokens = explode(' ', $text);
+  if (empty($tokens))
+    return true;
+
+  $input_cmd = $tolens[0];
+
   $cmds = get_commands();
   foreach ($cmds as &$cmd)
   {
-    if (!starts_with($text, $cmd->command) && !empty($cmd->command))
+    if (($input_cmd !== $cmd->command) && !empty($cmd->command))
       continue;
     
     $granted = $user->is_admin || !$cmd->admin;
